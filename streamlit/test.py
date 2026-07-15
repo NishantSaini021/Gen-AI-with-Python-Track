954b2b02b5f10981f9ddd3db58649478730f4672
Nishant Saini <Nishantsaini021.work@gmail.com>
2026-07-15 22:45:31 +0530

    Streamlit basics practice

--- streamlit/test.py
+++ streamlit/test.py
@@ -10,3 +10,24 @@ st.write(f"You selected {y} Excellent choice")
 st.checkbox("Confirm")
 if st.button("Confirm"):
     st.success("Your number was succesfuly selected")
+
+n_type = st.radio("Select Type of Number: ",["Natural", "Complex", "Integer"])
+if n_type:
+    st.success(f"You Selected: {n_type}")
+s = st.selectbox("Choose Required: ", ["A", "B", "C", "D", "E"])
+st.slider("Slider:", 1, 200)
+age = st.slider("Select Age", 0, 100)
+st.write(age)
+
+ia = st.number_input("Your Age: ", min_value=18, max_value=120)
+name = st.text_input("Enter Your Name: ")
+if name:
+    st.write(f"Thanks {name}")
+
+from datetime import date
+dob = st.date_input("Select your Date of birth: ", min_value = date(1900,1, 1))
+today = date.today()
+age = today.year - dob.year
+if (today.month, today.day) < (dob.month, dob.day):
+    age -= 1
+st.write(f"Your age is {age}")
